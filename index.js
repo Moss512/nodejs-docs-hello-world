0924df71f9b362532849beee388f7d99b341710f
Moss512 <94126291+Moss512@users.noreply.github.com>
2021-11-11 10:06:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World using automated builds with Jenkins Webhooks!");
+    response.end("Hello World using automated builds with Jenkins Webhooks + Docker!");
 });
 
 const port = process.env.PORT || 1337;
